a5828b688664616d8265ed37420991604595e12b
Claudio Reyes Miranda <claudio.reyes.miranda@gmail.com>
2016-10-15 06:07:31 -0300

    ordena conector MELI
    ordena conector MELI

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,15 @@
 angular.module('starter.controllers', [])
 
 //Controlador para login de MercadoLibre
-.controller('LoginMELI', function($scope) {
-  response.redirect(meliObject.getAuthURL(redirect_uri))
+.controller('LoginMELI', function($scope, LoginMELIService, $ionicPopup, $state) {
+    LoginMELIService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+            $state.go('tab.dash');
+        }).error(function(data) {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Login failed!',
+                template: 'Please check your credentials!'
+            });
+        });
 })
 
 .controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
--- www/js/services.js
+++ www/js/services.js
@@ -22,4 +22,34 @@ angular.module('starter.services', [])
             return promise;
         }
     }
+})
+
+.service('LoginMELIService', function($q) {
+    return {
+        loginUser: function() {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+
+            //Conecta appi MercadoLibre para login
+            var meliObject = new meli.Meli(1213949921402143, 'VT2tlhth0TLDe99pojRvhtb4szSSVhhq');
+            var redirect_uri = 'http://localhost:8100/success';
+            var redirect_uri_success = 'http://localhost:8100';
+            var callback = function(){
+                deferred.resolve('Bienvenido a Confidence!');
+            }
+            //fin conector MercadoLibre para login 
+
+            $state.go(meliObject.getAuthURL(redirect_uri));
+
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
 });
