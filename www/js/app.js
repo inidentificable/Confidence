22f0add6d20178a9ee875847751c89b10c498ea2
Sebastian Marquez <sebastianmar2587@gmail.com>
2016-10-15 02:32:29 -0300

    confidence login
    creacion login

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,11 @@
-<// Ionic Starter App
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', ]);
-
-
-
-app.run(function($ionicPlatform) {
+.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -23,31 +14,18 @@ app.run(function($ionicPlatform) {
   });
 })
 
-app.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/index.html',
-    controller: 'AppCtrl'
+  .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
   })
-
-  .state('app.perfil', {
-    url: '/perfil',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/perfil.html'
-      }
-    }
-  })
-
-  
-    
-
-  
-  });
-  // if none of the above states are matched, use this as the fallback
-  ///app/playlists
-  $urlRouterProvider.otherwise('');
+  $urlRouterProvider.otherwise('/login');
 });
